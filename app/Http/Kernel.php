1bfd6c2267ae9f8a390f237507ddee9d7554a1f1
bartoszderen97 <bartonia@wp.pl>
2020-05-08 22:32:29 +0200

    dodano cors header

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\Cors;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -18,6 +19,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
+        \App\Http\Middleware\Cors::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
@@ -60,6 +62,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'cors' => \App\Http\Middleware\Cors::class
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -12,17 +12,20 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::middleware('cors')->group(function () {
 
-Route::prefix('auth')->group(function() {
-    Route::post('login', 'API\AuthController@login');
-    Route::post('register', 'API\AuthController@register');
-    Route::post('resetPassword', 'API\AuthController@resetPassword')->name('password.reset');
-});
+    Route::prefix('auth')->group(function () {
+        Route::post('login', 'API\AuthController@login');
+        Route::post('register', 'API\AuthController@register');
+        Route::post('resetPassword', 'API\AuthController@resetPassword')->name('password.reset');
+    });
 
-Route::prefix('examination')->group(function() {
-    Route::get('get', 'API\ExaminationsController@getExamination');
-    Route::get('getByName/{name}', 'API\ExaminationsController@getExaminationByName');
-    Route::post('add', 'API\ExaminationsController@addExamination');
-    Route::put('edit', 'API\ExaminationsController@editExamination');
-    Route::delete('get', 'API\ExaminationsController@deleteExamination');
+    Route::prefix('examination')->group(function () {
+        Route::get('get', 'API\ExaminationsController@getExamination');
+        Route::get('getByName/{name}', 'API\ExaminationsController@getExaminationByName');
+        Route::post('add', 'API\ExaminationsController@addExamination');
+        Route::put('edit', 'API\ExaminationsController@editExamination');
+        Route::delete('get', 'API\ExaminationsController@deleteExamination');
+    });
+
 });
