0acd938faf6e5e580d9386b28fa79758c0cdaceb
bartoszderen97 <bartonia@wp.pl>
2020-05-18 17:03:33 +0200

    dodanie tabel zgłoszeń na badania i wyników

---
+++ database/migrations/2020_05_18_140732_create_application_for_examination_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApplicationForExaminationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('application_for_examination', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('patient_id')->unsigned();
+            $table->bigInteger('examination_id')->unsigned();
+            $table->bigInteger('applied_by_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('patient_id')
+                ->references('id')->on('users')
+                ->onDelete('CASCADE');
+            $table->foreign('applied_by_id')
+                ->references('id')->on('users')
+                ->onDelete('CASCADE');
+            $table->foreign('examination_id')
+                ->references('id')->on('examinations')
+                ->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('application_for_examination');
+    }
+}
---
+++ database/migrations/2020_05_18_141115_create_examination_results_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExaminationResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('examination_results', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->boolean('illness_presence');
+            $table->string('unit_name')->nullable();
+            $table->decimal('result_value')->nullable();
+            $table->decimal('result_lowest_norm')->nullable();
+            $table->decimal('result_highest_norm')->nullable();
+            $table->bigInteger('added_by')->unsigned();
+            $table->bigInteger('application_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('added_by')
+                ->references('id')->on('users')
+                ->onDelete('CASCADE');
+            $table->foreign('application_id')
+                ->references('id')->on('application_for_examination')
+                ->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('examination_results');
+    }
+}
