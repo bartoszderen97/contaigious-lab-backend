7be42a3f7790d6a45eb76b982f2452858261615d
bartoszderen97 <bartonia@wp.pl>
2020-06-08 15:23:31 +0200

    poprawiono jeszcze jeden błąd w dostępie do użytkowników

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -150,7 +150,7 @@ class UserController extends Controller
             return response()->json($data, $data['status']);
         }
         $userRole = Auth::user()->role;
-        if($userRole != "admin" || Auth::id() != $data['id_user']) {
+        if($userRole != "admin" && Auth::id() != $data['id_user']) {
             $data = [
                 'status' => Response::HTTP_FORBIDDEN,
                 'message' => 'You don\'t have permission to this page'
